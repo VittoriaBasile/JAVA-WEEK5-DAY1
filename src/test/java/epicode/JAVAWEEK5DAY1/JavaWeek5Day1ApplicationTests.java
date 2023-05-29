ede8c85b8f27c5fd796a2e2eacf0b456f15bfe09
VittoriaBasile <vichy.basile@gmail.com>
2023-05-29 13:33:05 +0200

    impostazioni iniziali

---
+++ src/test/java/epicode/JAVAWEEK5DAY1/JavaWeek5Day1ApplicationTests.java
@@ -0,0 +1,13 @@
+package epicode.JAVAWEEK5DAY1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWeek5Day1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
